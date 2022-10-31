9331bfa2595b2304eaace6fd5a9a69482eba0e20
소히 <103114936+sohee063@users.noreply.github.com>
2022-11-01 00:25:23 +0900

    <feat> Input 컴포넌트 구현 <feat> login 페이지 : 유효성 검사(id, password 정규표현식) (#2)

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,35 @@
 import Link from 'next/link';
 import type { NextPage } from 'next';
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled from 'styled-components';
+import TextInput from '../components/TextInput';
 
 const LoginPage: NextPage = () => {
+  const idRef = useRef();
+  const passwordRef = useRef();
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const [idErrMsg, setIdErrMsg] = useState('');
+  const [passwordErrMsg, setPasswordErrMsg] = useState('');
+
+  const isIdValidInput = /^[a-zA-Z0-9\`~!@#$%^&*()-_=+]{5,30}$/.test(id);
+  const isPasswordValidInput =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\\d`~!@#$%^&*()-_=+]{8,30}$/.test(password);
+
+  const loginIdRequestHandler = () => {
+    !isIdValidInput ? setIdErrMsg('올바른 아이디 형식으로 입력해주세요.') : setIdErrMsg('');
+  };
+  const loginPasswordRequestHandler = () => {
+    !isPasswordValidInput
+      ? setPasswordErrMsg('올바른 비밀번호 형식으로 입력해주세요.')
+      : setPasswordErrMsg('');
+  };
+
+  useEffect(() => {
+    loginIdRequestHandler();
+    loginPasswordRequestHandler();
+  }, [id, password]);
+
   return (
     <>
       <Header>
@@ -16,10 +42,31 @@ const LoginPage: NextPage = () => {
       </Header>
       <Form>
         <div>아이디</div>
-        <TextInput type='text' />
+        <div ref={idRef}>
+          <TextInput
+            type='text'
+            value={id || ''}
+            handlerValueState={(e) => setId(e.target.value)}
+            err={!isIdValidInput}
+            handlerOnblur={() => handleKeyPress()}
+          />
+        </div>
+
+        <p className='err'>{idErrMsg}</p>
         <div>비밀번호</div>
-        <TextInput type='password' />
-        <LoginButton disabled>로그인</LoginButton>
+        <div ref={passwordRef}>
+          <TextInput
+            type='password'
+            value={password || ''}
+            handlerValueState={(e) => setPassword(e.target.value)}
+            err={!isPasswordValidInput}
+          />
+        </div>
+
+        <p className='err'>{passwordErrMsg}</p>
+        <LoginButton disabled={!isIdValidInput || !isPasswordValidInput ? true : false}>
+          로그인
+        </LoginButton>
       </Form>
     </>
   );
@@ -43,10 +90,20 @@ const Form = styled.div`
   flex-direction: column;
   margin-top: 40px;
   padding: 0 20px 40px;
-`;
 
-const TextInput = styled.input`
-  border: 1px solid #000;
+  > div:first-child {
+    font-weight: 700;
+    font-size: 13px;
+    color: #6c6c7d;
+  }
+
+  > p {
+    display: none;
+    margin-top: 8px;
+    font-weight: 400;
+    font-size: 13px;
+    color: #ed4e5c;
+  }
 `;
 
 const LoginButton = styled.button`
