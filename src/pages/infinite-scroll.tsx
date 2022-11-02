a0382f239d59a4a92b3fe9673d5ca3b6d2dd19b5
sohee kim <sohee063@nate.com>
2022-11-02 19:59:25 +0900

    <feat> infinite scroll 페이지 : 무한스크롤 구현

--- src/pages/infinite-scroll.tsx
+++ src/pages/infinite-scroll.tsx
@@ -1,12 +1,47 @@
 import Link from 'next/link';
 import type { NextPage } from 'next';
-import React from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import styled from 'styled-components';
+import { useInView } from 'react-intersection-observer';
 
 import products from '../api/data/products.json';
 import ProductList from '../components/ProductList';
+import { useDispatch, useSelector } from 'react-redux';
+import { getScrollProductList } from '../redux/actions/productActions';
 
 const InfiniteScrollPage: NextPage = () => {
+  const [page, setPage] = useState(1);
+  const [fetchData, setFetchData] = useState([]);
+  const dispatch = useDispatch();
+  const { scrollProductList, totalCount } = useSelector((state) => state.product);
+
+  const { ref, inView } = useInView({
+    threshold: 1,
+  });
+
+  const fakeFetch = (delay = 300) => new Promise((res) => setTimeout(res, delay));
+
+  const fetchMoreData = async () => {
+    setPage(page + 1);
+    await fakeFetch();
+    if (fetchData.length < totalCount) {
+      dispatch(getScrollProductList(page));
+    }
+  };
+
+  useEffect(() => {
+    if (!inView) {
+      return;
+    }
+    fetchMoreData();
+  }, [inView]);
+
+  useEffect(() => {
+    dispatch(getScrollProductList(1));
+  }, []);
+
+  console.log(scrollProductList, page);
+
   return (
     <>
       <Header>
@@ -18,8 +53,9 @@ const InfiniteScrollPage: NextPage = () => {
         </Link>
       </Header>
       <Container>
-        <ProductList products={products} />
+        <ProductList products={scrollProductList} />
       </Container>
+      <Scroll ref={ref}></Scroll>
     </>
   );
 };
@@ -42,4 +78,8 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 0 20px 40px;
+`;
+
+const Scroll = styled.div`
+  height: 200px;
 `;
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,12 +1,14 @@
 import {
   GET_ALL_PRODUCTS_SUCCESS,
   GET_DETAIL_PRODUCTS_SUCCESS,
+  GET_SCROLL_PRODUCTS_SUCCESS,
   PRODUCT_LOADING,
   PRODUCT_ERROR,
 } from '../actions/productActions';
 
 const initialstate = {
   allProductList: [],
+  scrollProductList: [],
   productDetail: [],
   totalCount: '',
   loading: '',
@@ -15,12 +17,19 @@ const initialstate = {
 
 const productReducer = (state = initialstate, action) => {
   let { type, payload } = action;
+  console.log('페이로드', payload);
   switch (type) {
     case GET_ALL_PRODUCTS_SUCCESS:
       return {
         ...state,
         allProductList: payload.allProductList.data.products,
         totalCount: payload.allProductList.data.totalCount,
+      };
+    case GET_SCROLL_PRODUCTS_SUCCESS:
+      return {
+        ...state,
+        scrollProductList: [...state.scrollProductList, ...payload.scrollProductList.data.products],
+        totalCount: payload.scrollProductList.data.totalCount,
       };
     case GET_DETAIL_PRODUCTS_SUCCESS:
       return {
